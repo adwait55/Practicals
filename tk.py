9ece9d7354bd8f1538e8e8e228d937bce72913da
Anshul Chauhan <djgammasonic@gmail.com>
2015-07-01 11:10:12 +0530

    Update tk.py

--- tk.py
+++ tk.py
@@ -1,31 +1,37 @@
+import Tkinter as tk
 from Tkinter import *
-master = Tk() #to solve StringVar() problem, this has to be at the beginning
+master = Tk()
 v = StringVar()
 b = StringVar()
 
-def show_entry_fields():
-   print("First Name: %s\nLast Name: %s" % (e1.get(), e2.get()))
-   e1.delete(0,END)
-   e2.delete(0,END)
+def refresh():
+	import q
+	q.d()
 
 def check():
 	if(e1.get() == e2.get()):
-		print("WOW")
-	
+		w = Label(master, text=" -- CORRECT -- ")
+		w.grid(row=4, column=2, sticky=W, pady=4)
+	else:
+		w = Label(master, text="** WRONG **")
+		w.grid(row=4, column=2, sticky=W, pady=4)
 
-Label(master, text="Enter here:").grid(row=0)
-Label(master, text="Last Name").grid(row=1)
+#Label(master, text="yo").grid(row=1)
 
 e1 = Entry(master, textvariable=v)
 e2 = Entry(master)
 e1.insert(10,"Miller")
-e2.insert(10,"Jill")
+e2.insert(10,"Enter here")
 
-e1.grid(row=0, column=1)
-e2.grid(row=1, column=1)
+e1.grid(row=1, column=1)
 
 Button(master, text='Quit', command=master.quit).grid(row=3, column=0, sticky=W, pady=4)
-Button(master, text='Show', command=show_entry_fields).grid(row=3, column=1, sticky=W, pady=4)
-Button(master, text='Show', command=check).grid(row=3, column=2, sticky=W, pady=4)
+Button(master, text='Refresh', command=refresh).grid(row=3, column=1, sticky=W, pady=4)
+Button(master, text='Check', command=check).grid(row=3, column=2, sticky=W, pady=4)
+
+
+a = Label(master, text="Enter the CAPTCHA!")
+a.grid(row=0, column=1, sticky=W, pady=4)
+
 
 mainloop( )
