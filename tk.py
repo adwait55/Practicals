547ce0db7c1e52bdafea8c7610a38ddb99f19620
Anshul Chauhan <djgammasonic@gmail.com>
2015-07-01 10:26:05 +0530

    Update tk.py
    Solved StringVar() problem by adding a Tk() instance at the brginning.

--- tk.py
+++ tk.py
@@ -1,15 +1,22 @@
-from tkinter import *
+from Tkinter import *
+master = Tk()
+v = StringVar()
+b = StringVar()
 
 def show_entry_fields():
    print("First Name: %s\nLast Name: %s" % (e1.get(), e2.get()))
    e1.delete(0,END)
    e2.delete(0,END)
 
-master = Tk()
-Label(master, text="First Name").grid(row=0)
+def check():
+	if(e1.get() == e2.get()):
+		print("WOW")
+	
+
+Label(master, text="Enter here:").grid(row=0)
 Label(master, text="Last Name").grid(row=1)
 
-e1 = Entry(master)
+e1 = Entry(master, textvariable=v)
 e2 = Entry(master)
 e1.insert(10,"Miller")
 e2.insert(10,"Jill")
@@ -19,5 +26,6 @@ e2.grid(row=1, column=1)
 
 Button(master, text='Quit', command=master.quit).grid(row=3, column=0, sticky=W, pady=4)
 Button(master, text='Show', command=show_entry_fields).grid(row=3, column=1, sticky=W, pady=4)
+Button(master, text='Show', command=check).grid(row=3, column=2, sticky=W, pady=4)
 
 mainloop( )
